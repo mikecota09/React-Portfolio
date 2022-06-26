455d33c22181ce560d1f105259bcbb9c539a9ecd
Michael Cota <mikecotahf@gmail.com>
2022-06-26 11:40:56 -0400

    updated footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,9 @@ import About from "./components/About/About";
 import Resume from "./components/Resume/Resume";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  // const [navigations] = useState([
-  //   {
-  //     name: 'About me',
-  //     description: 'Michael Cota bio',
-  //   },
-  //   { name: 'Portfolio', description: 'Work portfolio' },
-  //   { name: 'Contact', description: 'Contact me' },
-  //   { name: 'Resume', description: 'Download or view resume' },
-  // ]);
-
-  // const [currentNav, setCurrentNav] = useState(navigations[0]);
-
   return (
     <div className="app">
       <Topbar></Topbar>
@@ -49,6 +17,10 @@ function App() {
         <Portfolio></Portfolio>
         <Contact></Contact>
         <Resume></Resume>
+        <Footer></Footer>
+      </div>
+      <div className="footer">
+        <Footer></Footer>
       </div>
     </div>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./footer.scss";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Footer() {
+  return (
+    <div className="footer" id="footer">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/mikecota09"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon className="icon"></GitHubIcon>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/michael-cota-41677522b/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedInIcon className="icon"></LinkedInIcon>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/Mike_Cota09"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <TwitterIcon className="icon"></TwitterIcon>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
